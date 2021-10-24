85586b87ad57b15b6abe2d0593c8a78ea7bd2b76
rhoomse <rhoomse@gmail.com>
2021-10-24 12:00:53 +0300

    Disk+Page

---
+++ Disk.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hak2021v2._0
+{
+    class Disk
+    {
+        string path;
+
+        int capacity;
+        int pointer;
+        Page[] pages;
+
+        public Disk()
+        {
+        }
+        public void New(string path, int capacity)
+        { 
+            FileInfo disk = new FileInfo(path);
+            pages = new Page[capacity];
+            pointer = 0;
+        }
+        /*
+        private string ComputeMD5Checksum(string path)
+        {
+            using (FileStream fs = System.IO.File.OpenRead(path))
+            {
+                MD5 md5 = new MD5CryptoServiceProvider();
+                byte[] fileData = new byte[fs.Length];
+                fs.Read(fileData, 0, (int)fs.Length);
+                byte[] checkSum = md5.ComputeHash(fileData);
+                string result = BitConverter.ToString(checkSum).Replace("-", String.Empty);
+                return result;
+            }
+        }
+        */
+
+        ulong Set(byte[] data)
+        {
+            using (BinaryWriter bw = new BinaryWriter(File.Open(path, FileMode.Open)))
+            {
+                
+            }
+        }
+        Page.Note Get()
+        {
+
+        }
+        void Update()
+        {
+
+        }
+    }
+}
---
+++ Page.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hak2021v2._0
+{
+    class Page
+    {
+        public struct Note
+        {
+            int m_size;
+            byte[] data;
+        }
+        Note[] notes;
+        string hash;
+        Page(int count)
+        {
+            notes = new Note[count];
+        }
+        void Update()
+        {
+
+        }
+    }
+}
