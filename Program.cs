7b6e7f34bbd257c2b19c1925a9131ccc3cebd006
loinos <PavlovskiyD.V@yandex.ru>
2021-10-24 11:09:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hak2021v2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
