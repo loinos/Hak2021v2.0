93159f88eb9f1d294c457df56631267197c2cdeb
loinos <PavlovskiyD.V@yandex.ru>
2021-10-24 11:39:16 +0300

    add xor generator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,29 @@ namespace Hak2021v2._0
 {
     class Program
     {
+        public byte[] bitArrayConvertor(BitArray input_bits)
+        {
+            BitArray a = new BitArray(input_bits.Length + (input_bits.Length & 8));
+            for (int i = 0; i < input_bits.Length; i++)
+            {
+                a[i] = input_bits[i];
+            }
+            byte[] bytes = new byte[a.Length / 8];
+            a.CopyTo(bytes, 0);
+            return bytes;
+        }
+        public byte[] generateXOR(byte[] list1,byte[] list2)
+        {
+            BitArray list1_bits = new BitArray(list1);
+            BitArray list2_bits = new BitArray(list2);
+            BitArray ans_bits = new BitArray(list2_bits.Length);
+
+            for(int i = 0; i < ans_bits.Length; i++)
+            {
+                ans_bits[i] = list1_bits[i] ^ list2_bits[i];
+            }
+            return bitArrayConvertor(ans_bits);
+        }
         static void Main(string[] args)
         {
 
